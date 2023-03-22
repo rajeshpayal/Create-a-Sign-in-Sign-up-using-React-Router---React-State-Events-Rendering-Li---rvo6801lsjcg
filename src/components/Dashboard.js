aa527e64f49aff2266ae5e0f3a3c45ac14d7f136
rajeshpayal <rajeshpayal1094@gmail.com>
2023-03-22 19:26:09 +0530

    first

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,14 +1,20 @@
 import React from "react";
 
-
-function Dashboard({ setLoggedIn }) {
-
-    return (
-        <div id="dashboardPage">
-            <h1 id="dashboard-tag"> Welcome to Dashboard</h1>
-            <button id="dashboard-logout" onClick={() => setLoggedIn(false)}>Log out </button>
-        </div>
-    )
-
+function Dashboard({ setLoggedIn, setNavigate }) {
+  return (
+    <div id="dashboardPage">
+      <h1 id="dashboard-tag"> Welcome to Dashboard</h1>
+      <button
+        id="dashboard-logout"
+        onClick={() => {
+          console.log("You have logged out");
+          setLoggedIn(false);
+          setNavigate(true);
+        }}
+      >
+        Log out{" "}
+      </button>
+    </div>
+  );
 }
 export default Dashboard;
